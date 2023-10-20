e442150487aebdfab45481a6747c6da3dae76abf
Pablo Alan <pabloxt14@gmail.com>
2023-10-19 22:51:52 -0300

    feat: validated if user is real authenticated

--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -2,14 +2,23 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { useAuth } from "../hooks/auth";
 
+import { useEffect } from 'react';
+
 import { AdminRoutes } from './admin.routes';
 import { CustomerRoutes } from './customer.routes';
 import { SaleRoutes } from './sale.routes';
 import { AuthRoutes } from './auth.routes';
 import { USER_ROLE } from '../utils/role';
+import { api } from '../services/api';
 
 export function Routes() {
-  const { user } = useAuth();
+  const { user, signOut } = useAuth();
+
+  useEffect(() => {
+    api
+      .get("/users/validated", { withCredentials: true })
+      .catch(error => signOut())
+  }, [])
 
   function AccessRoutes() {
     switch(user.role) {
