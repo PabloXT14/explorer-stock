9338a73400ea1e1182a6b30310f88d4c6314b2b1
Pablo Alan <pabloxt14@gmail.com>
2023-10-19 22:21:57 -0300

    feat: using cookie on authorization routes

--- web/src/pages/SalesReport/index.jsx
+++ web/src/pages/SalesReport/index.jsx
@@ -1,6 +1,14 @@
+import { useEffect } from 'react';
 import { Container } from "./styles";
+import { api } from '../../services/api';
 
 export function SalesReport() {
+  useEffect(() => {
+    api
+      .get('/sales', { withCredentials: true }) // sempre usar o withCredentials para trabalhar com cookies entre front-end e back-end
+      .then(response => console.log(response.data))
+  }, [])
+
   return (
     <Container>
       <h1>Relatório de Vendas</h1>
